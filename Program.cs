590801f70b388ee2afd3f33b8925b80559da149e
Kyle Krick <krkrick@gmail.com>
2019-05-08 22:34:17 -0700

    added code to initialize storage account

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace blob_app
 {
@@ -6,7 +8,20 @@ namespace blob_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectionString = Environment.GetEnvironmentVariable("storageconnectionstring");
+
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(connectionString, out storageAccount))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("The environment variable storageconnectionstring is not set." 
+                    + "\nPlease set it and rerun this program.");
+                Console.WriteLine("Hit any key to exit this program");
+                Console.ReadLine();
+            }
         }
     }
 }
